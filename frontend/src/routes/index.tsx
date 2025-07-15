3686f4c89c375c3445c3933b1c89098e1a97bb4a
alvaroduart <alvarohenriqueduartemendes98@gmail.com>
2025-07-15 15:49:31 -0300

    agora vai

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -7,7 +7,6 @@ import PrivateRoute from './PrivateRoute';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
 import ContactPage from '../pages/ContactPage';
 import MovieDetailsPage from '../pages/MovieDetailsPage';
 
@@ -22,8 +21,7 @@ const AppRoutes: React.FC = () => {
         {/* Rotas públicas */}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/cadastro" element={<RegisterPage />} />
-        <Route path="/esqueci-senha" element={<ForgotPasswordPage />} />
+        <Route path="/cadastro" element={<RegisterPage />} />        
         <Route path="/contato" element={<ContactPage />} />
         <Route path="/filme/:id" element={<MovieDetailsPage />} />
         
